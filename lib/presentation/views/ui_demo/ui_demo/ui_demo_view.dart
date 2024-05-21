05949b1998e908de912e9e260fcded6763ecc17b
Ittipol Kaewprasert <Ittipol.Kaewprasert@krungsrinimble.com>
2024-05-21 12:40:47 +0700

    Code improve

--- lib/presentation/views/ui_demo/ui_demo/ui_demo_view.dart
+++ lib/presentation/views/ui_demo/ui_demo/ui_demo_view.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/presentation/views/common/blank_page_widget.dart';
 import 'package:flutter_demo/presentation/views/ui_demo/ui_demo/components/component_article_list/component_article_list_view.dart';
@@ -26,12 +27,42 @@ class _UiDemoView  extends ConsumerState<UiDemoView> {
         physics: const ClampingScrollPhysics(),
         child: Column(
           children: [
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 400.h,
+                viewportFraction: 1
+              ),
+              items: [1,2,3,4,5].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      width: MediaQuery.sizeOf(context).width,
+                      margin: EdgeInsets.symmetric(horizontal: 5.w),
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary.withAlpha(200)
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Title $i",
+                          style: TextStyle(
+                            fontSize: 24.sp,
+                            overflow: TextOverflow.ellipsis,
+                            color: Colors.white
+                          )
+                        ),
+                      )
+                    );
+                  },
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 16.h),
             const ComponentOneView(),
             SizedBox(height: 16.h),
             const ComponentDetailListView()
           ],
         ),
-      )      
+      )
     );
 
   }
