02e2d5ce908c02b497f4b800f60dfa8da2dc1aaa
Ittipol Kaewprasert <Ittipol.Kaewprasert@krungsrinimble.com>
2025-01-19 12:31:55 +0700

    Certificate pinning code update

--- lib/config/network/dio_option.dart
+++ lib/config/network/dio_option.dart
@@ -8,11 +8,6 @@ import 'package:http_certificate_pinning/http_certificate_pinning.dart';
 
 class DioOption {
 
-  List<String> allowedSHAFingerprints = [
-    "99 8E 63 B9 2D F2 2C BD 24 34 02 58 96 5C 19 27 7E 9D 71 2F 8D 0B E2 6B D4 CE 7D BF 8E E1 C4 44", // localhost:5050
-    "00 FF AA D8 CC 6C 3A B4 CC 7D F5 0E A1 C1 0C 51 61 0D 96 86 21 65 24 7A CA 7A F3 AF E2 CE A0 2D" // pokeapi.co
-  ];
-
   List<String> exceptUrlList = [
     "https://localhost:5050"
   ];
@@ -31,7 +26,7 @@ class DioOption {
 
     if(checkCertificatePinning && !_isExceptUrl(exceptUrlList, baseUrl)) {
       // Cannot use with self-signed certificate
-      dio.interceptors.add(CertificatePinningInterceptor(allowedSHAFingerprints: allowedSHAFingerprints));
+      dio.interceptors.add(CertificatePinningInterceptor(allowedSHAFingerprints: Certificate.allowedSHAFingerprints));
     }else {
       (dio.httpClientAdapter as IOHttpClientAdapter).createHttpClient = () {
         final client = HttpClient();
--- lib/data/app/certificate.dart
+++ lib/data/app/certificate.dart
@@ -1,4 +1,10 @@
 class Certificate {
+
+  static const List<String> allowedSHAFingerprints = [
+    "99 8E 63 B9 2D F2 2C BD 24 34 02 58 96 5C 19 27 7E 9D 71 2F 8D 0B E2 6B D4 CE 7D BF 8E E1 C4 44", // localhost:5050
+    "00 FF AA D8 CC 6C 3A B4 CC 7D F5 0E A1 C1 0C 51 61 0D 96 86 21 65 24 7A CA 7A F3 AF E2 CE A0 2D" // pokeapi.co
+  ];
+
   static String? _certificate;
 
   static String get certificate => _certificate ?? "";
