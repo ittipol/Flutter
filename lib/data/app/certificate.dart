3a09a4616ab342ebfb93f2767e14718e7ddd49b9
Ittipol Kaewprasert <Ittipol.Kaewprasert@krungsrinimble.com>
2025-01-21 18:58:10 +0700

    Update certificate files & SSL certificate fingerprint

--- lib/data/app/certificate.dart
+++ lib/data/app/certificate.dart
@@ -1,7 +1,7 @@
 class Certificate {
 
   static const List<String> allowedSHAFingerprints = [
-    "99 8E 63 B9 2D F2 2C BD 24 34 02 58 96 5C 19 27 7E 9D 71 2F 8D 0B E2 6B D4 CE 7D BF 8E E1 C4 44", // localhost:5050
+    "9F 70 BD DC 76 4C 5F 6D F5 D3 40 64 D8 62 99 C9 05 6F DE 7C 6D 1C 81 59 E2 D5 BD C2 44 BF 58 1E", // localhost:5050
     "00 FF AA D8 CC 6C 3A B4 CC 7D F5 0E A1 C1 0C 51 61 0D 96 86 21 65 24 7A CA 7A F3 AF E2 CE A0 2D" // pokeapi.co
   ];
 
